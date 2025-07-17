b23e7eba3c273487d2df586a45f50250f7a3425f
JiYoSu <jiyong.sugimoto@outlook.com>
2025-07-17 01:09:09 +0000

    e

---
+++ src/main/java/org/frc6423/robot/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,15 @@
+package org.frc6423.robot.subsystems.intake;
+
+public class IntakeConstants {
+    public static final int PIVOT_CAN_ID = 12;
+    public static final String PIVOT_CAN_BUS = "main";
+
+    public static final int PIVOT_ABS_ENCODER_CAN_ID = 13;
+    public static final String PIVOT_ABS_ENCODER_CAN_BUS = "main";
+
+    public static final int ROLLER_CAN_ID = 14;
+    public static final String ROLLER_CAN_BUS = "main";
+
+    public static final int ROLLER_ABS_ENCODER_CAN_ID = 15;
+    public static final String ROLLER_ABS_ENCODER_CAN_BUS = "main";
+}
